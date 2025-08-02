038b2abb21d4d4203e6e0ca21895301b5a685eb1
Sascha001 <149436970+Sascha001@users.noreply.github.com>
2025-08-02 19:01:52 +0200

    Revert "Adjust sidebar dimensions for better space utilization"
    This reverts commit 46a0a8864bd7eb94cba12b92fa652296a5723bae.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,14 +44,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SidebarProvider
-          style={
-            {
-              "--sidebar-width": "20rem",
-              "--sidebar-width-icon": "4rem",
-            } as React.CSSProperties
-          }
-        >
+        <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
             <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
